0a308b3f6fea182ab519fe1df725ba08324f1841
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-03 17:48:48 -0700

    document ready and styling

--- src/script/index.jsx
+++ src/script/index.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './components/app';
 
-if(document.body.contains(document.getElementById("convo-form"))){
-  var mainForm = document.getElementById("convo-form")
-  mainForm.style.display = "none";
-  var cfw = document.createElement('div');
-  cfw.id = 'convo-form-window';
-  document.body.appendChild(cfw);
 
-  render(
-    <App />,
-    document.getElementById('convo-form-window')
-  );
-}
+var tid = setInterval( function () {
+    if ( document.readyState !== 'complete' ) return;
+    clearInterval( tid );
+    // do your work
+    if(document.body.contains(document.getElementById("convo-form"))){
+      var mainForm = document.getElementById("convo-form")
+      mainForm.style.display = "none";
+      var cfw = document.createElement('div');
+      cfw.id = 'convo-form-window';
+      mainForm.parentNode.insertBefore(cfw, mainForm.nextSibling);
+
+      render(
+        <App />,
+        document.getElementById('convo-form-window')
+      );
+    }
+}, 10 );
